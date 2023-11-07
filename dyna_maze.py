bd8819f014be8d2dcc3b4ffc8b2ab50156b8b7a9
tmulroy <thomasjohnmulroy@gmail.com>
2023-11-07 10:49:24 -0500

    complete DynaMaze programming exercise

--- dyna_maze.py
+++ dyna_maze.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 # Setup Grid
 grid = np.ones((6, 9))
-print(grid)
 grid[1:4, 2] = 0
 grid[0:3, 7] = 0
 grid[4, 5] = 0
@@ -12,14 +11,11 @@ goal = (0, 8)
 rewards = np.zeros((6, 9))
 rewards[goal] = 1
 
-# Parameters
+# DynaQ Algorithm Parameters
 gamma = 0.95
 alpha = 0.1
 epsilon = 0.1
-n = [0,5,50]
-numSteps = 0
-numEpisodes = 50
-stepsPerEpisode = []
+n = [0, 5, 50]
 
 # Actions
 '''
@@ -30,11 +26,15 @@ stepsPerEpisode = []
 '''
 actions = [0,1,2,3]
 
-# Helpers
+# Helper Variables
 rng = np.random.default_rng(seed=42)
 observedStates = []
 observedStateActions = np.zeros((grid.size, len(actions)))
-
+numSteps = 0
+numEpisodes = 50
+numExperiments = 30
+avgNumSteps = np.empty((numExperiments, numEpisodes))
+avgResults = np.empty((len(n), numEpisodes))
 
 # Q-Table
 # States x Actions
@@ -43,6 +43,7 @@ Q = np.zeros((grid.size, len(actions)))
 # Model
 # States x Actions
 Model = np.zeros((grid.size, len(actions)), dtype=object)
+
 
 # Epsilon-Greedy Policy
 def epsilon_greedy(S):
@@ -54,6 +55,7 @@ def epsilon_greedy(S):
         max = np.argwhere(Q[S] == np.amax(Q[S]))
         action = rng.choice(max)[0]
     return action
+
 
 def not_out_of_grid(S, A):
     notValidMoves = {
@@ -86,10 +88,10 @@ def not_out_of_grid(S, A):
     }
 
     if S in notValidMoves.keys() and A in notValidMoves[S]:
-        print(f'not valid move')
         return False
     else:
         return True
+
 
 def take_action(S, A):
     nextState = 0
@@ -98,7 +100,6 @@ def take_action(S, A):
     if not_out_of_grid(S,A):
 
         if A == 0:  # Up
-            # if using 2D indices: nextState = np.unravel_index(S, grid.shape)
             nextState = S - grid.shape[1]
         elif A == 1:  # Right
             nextState = S + 1
@@ -122,65 +123,78 @@ def take_action(S, A):
     return R, nextState
 
 
-for episode in range(numEpisodes):
-    # DynaQ Algorithm
-    currentState = np.ravel_multi_index(start, grid.shape)
-    numSteps = 0
-    while True:
-        if currentState == np.ravel_multi_index(goal, grid.shape):
-            print('Reached Terminal State')
-            break
-        else:
-            S = currentState
-            A = epsilon_greedy(S)
-            observedStateActions[S, A] = 1
-            if not S in observedStates:
-                observedStates.append(S)
+# Run 30 experiments for n=0,5,50 with 50 episodes each experiment.
+for idx, numPlanningSteps in enumerate(n):
 
-            # Take action A; observe resultant reward, R and state, S'
-            R, nextState = take_action(S, A)
-            print('\n')
-            print(f'currentState: {S}')
-            print(f'Action: {A}')
-            print(f'nextState: {nextState}')
-            print(f'Reward: {R}')
+    for experiment in range(numExperiments):
 
-            # Update Q-Table
-            max = np.argwhere(Q[nextState] == np.amax(Q[nextState]))
-            greedyAction = rng.choice(max)[0]
-            Q[S, A] = Q[S, A] + alpha*(R + gamma*Q[nextState, greedyAction] - Q[S, A])
+        Q = np.zeros((grid.size, len(actions)))
+        stepsPerEpisode = []
 
-            # Update Model (assuming deterministic environment)
-            Model[S, A] = (R, nextState)
+        for episode in range(numEpisodes):
 
-            # Planning Phase
-            print(f'Q[S,A]: {Q[S,A]}')
-            for i in range(n):
-                print(f'    Planning Phase')
-                print(f'      observedStates: {observedStates}')
-                S = rng.choice(observedStates)
-                observedActions = np.argwhere(observedStateActions[S] == np.amax(observedStateActions[S]))
-                A = rng.choice(observedActions)[0]
-                R, nextStatePlanning = Model[S, A]
-                print(f'      S: {S}')
-                print(f'      A: {A}')
-                print(f'      nextState: {nextStatePlanning}')
-                print(f'      R: {R}')
+            # DynaQ Algorithm
+            currentState = np.ravel_multi_index(start, grid.shape)
+            numSteps = 0
+            while True:
+                if currentState == np.ravel_multi_index(goal, grid.shape):
+                    # print('Reached Terminal State')
+                    break
+                else:
+                    S = currentState
+                    A = epsilon_greedy(S)
+                    observedStateActions[S, A] = 1
+                    if not S in observedStates:
+                        observedStates.append(S)
 
-                # Update Q-Table
-                greedyActions = np.argwhere(Q[nextStatePlanning] == np.amax(Q[nextStatePlanning]))
-                greedyAction = rng.choice(greedyActions)[0]
-                Q[S, A] = Q[S, A] + alpha * (R + gamma * Q[nextStatePlanning, greedyAction] - Q[S, A])
-                print(f'      Q[S,A]: {Q[S,A]}')
+                    # Take action A; observe resultant reward, R and state, S'
+                    R, nextState = take_action(S, A)
+                    # print('\n')
+                    # print(f'currentState: {S}')
+                    # print(f'Action: {A}')
+                    # print(f'nextState: {nextState}')
+                    # print(f'Reward: {R}')
 
-            currentState = nextState
-            numSteps += 1
+                    # Update Q-Table
+                    max = np.argwhere(Q[nextState] == np.amax(Q[nextState]))
+                    greedyAction = rng.choice(max)[0]
+                    Q[S, A] = Q[S, A] + alpha*(R + gamma*Q[nextState, greedyAction] - Q[S, A])
 
-    stepsPerEpisode.append(numSteps)
+                    # Update Model (assuming deterministic environment)
+                    Model[S, A] = (R, nextState)
+
+                    # Planning Phase
+                    for i in range(numPlanningSteps):
+                        # print(f'    Planning Phase')
+                        # print(f'      observedStates: {observedStates}')
+                        S = rng.choice(observedStates)
+                        observedActions = np.argwhere(observedStateActions[S] == np.amax(observedStateActions[S]))
+                        A = rng.choice(observedActions)[0]
+                        R, nextStatePlanning = Model[S, A]
+                        # print(f'      S: {S}')
+                        # print(f'      A: {A}')
+                        # print(f'      nextState: {nextStatePlanning}')
+                        # print(f'      R: {R}')
+
+                        # Update Q-Table
+                        greedyActions = np.argwhere(Q[nextStatePlanning] == np.amax(Q[nextStatePlanning]))
+                        greedyAction = rng.choice(greedyActions)[0]
+                        Q[S, A] = Q[S, A] + alpha * (R + gamma * Q[nextStatePlanning, greedyAction] - Q[S, A])
+                    currentState = nextState
+                    numSteps += 1
+
+            stepsPerEpisode.append(numSteps)
+        avgNumSteps[experiment] = stepsPerEpisode
+    avgResults[idx] = np.mean(avgNumSteps, axis=0)
+
 
 print(f'Q: {Q}')
-print(f'numSteps: {numSteps}')
-print(f'steps per episode: {stepsPerEpisode}')
 
-plt.plot(range(numEpisodes), stepsPerEpisode)
+# Plot Learning Curves for n=0,5,50
+plt.xlabel('Episodes')
+plt.ylabel('Steps per Episode')
+plt.plot(range(numEpisodes), avgResults[0], label='0 planning steps')
+plt.plot(range(numEpisodes), avgResults[1], label='5 planning steps')
+plt.plot(range(numEpisodes), avgResults[2], label='50 planning steps')
+plt.legend(loc='upper center')
 plt.show()
